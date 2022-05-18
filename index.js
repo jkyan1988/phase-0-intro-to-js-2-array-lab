a52f03fd653c03f10716ad798a1f7fc3dc670bca
James Kyan <james.kyan88@gmail.com>
2022-05-18 19:52:52 -0400

    commit tree 4630f72aca549e993dac979444fca5b3ef8729d0

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    name = cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    name = cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    name = cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    name = cats.shift(name);
+}
+
+function appendCat(name){
+    const CopyCats = [...cats]
+    CopyCats.push(name);
+    return CopyCats;
+}
+
+function prependCat(name){
+    const CopyCats = [...cats]
+    CopyCats.unshift(name);
+    return CopyCats;
+}
+
+function removeLastCat(name){
+    const CopyCats = [...cats]
+    CopyCats.pop(name);
+    return CopyCats;
+}
+
+function removeFirstCat(name){
+    const CopyCats = [...cats]
+    CopyCats.shift(name);
+    return CopyCats;
+}
